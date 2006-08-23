14ae37bea248eb07a565f21cfeba9f0bd823a709
Clive Crous <clive@crous.co.za>
2006-08-23 22:38:29 +0000

    [darcs @ adjust prism example to fit into view]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-26 21:21:26

--- examples/objects_finitesolidprimitives_prism.rb
+++ examples/objects_finitesolidprimitives_prism.rb
@@ -17,7 +17,8 @@ prism =
         ] )
 
 prism << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
-prism << Povray::Methods::MultiValue.new( [0.2], 'scale' )
+prism << Povray::Methods::Scale.new( 0.15 )
+prism << Povray::Methods::Translate.new( Povray::DataTypes::Vector::XYZ.new(-0.2,0,-0.2) )
 
 scene << prism
 
