39f439a700c02e06f06ab581c27f3771ac92fa07
Clive Crous <clive@crous.co.za>
2006-08-23 22:35:49 +0000

    [darcs @ tweak test1 example into a presentable state]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-01-08 18:51:43

--- examples/test1.rb
+++ examples/test1.rb
@@ -1,40 +1,53 @@
 require 'povray.rb'
 include Povray
+
 include Objects
 include FiniteSolidPrimitives
+include FinitePatchPrimitives
+
 include Atmosphere
 include Methods
 include LightSources
 include Textures
 
-maingroup = Povray::Group.new()
-
-background = Background.new( Colour.new( "Black" ) )
-
-camera = Camera::Basic.new( Location.new( [0, 50, 0] ), LookAt.new( [0, 1, 2] ) )
-
-light = PointLight.new( Vector.new( [12,14,-13] ), Colour.new( "White" ) )
-
-maingroup << '#include "colors.inc"'
-maingroup << background
-maingroup << camera
-
+spheres = CSG::Merge.new()
 radius = 20.0
 
 (0...720).each do |deg|
-    degrees = deg*2
-    sphere = Sphere.new([degrees/50.0-12.5, 1+radius*Math::cos(Math::PI*degrees/180.0), 2+radius*Math::sin(Math::PI*degrees/180.0)], 0.5)
+    degrees = deg*4
+    sphere = Sphere.new([degrees/100.0-12.5, 1+radius*Math::cos(Math::PI*degrees/180.0), 2+radius*Math::sin(Math::PI*degrees/180.0)], (radius.abs)/20)
 
     texture = Texture.new()
-    pigment = Pigments::SolidColour.new( Colour.new( ColourRGB.new( [ degrees/720.0,1,0.5 ] ) ) )
-    texture << pigment
+    texture << Pigments::SolidColour.new( Colour.new( ColourRGBT.new( [ degrees/720.0,1,0.5,0.85 ] ) ) )
 
     sphere << texture
 
-    maingroup << sphere
+    spheres << sphere
     radius -= 0.05
 end
 
-maingroup << light
+spheres << "interior { ior Ruby_Ior }"
 
-print maingroup
+scene = Povray::Group.new()
+
+scene << '#include "colors.inc"'
+scene << '#include "textures.inc"'
+scene << '#include "glass.inc"'
+
+scene << Background.new( Colour.new( "Black" ) )
+scene << Camera::Basic.new( Location.new( [15, 15, 20] ), LookAt.new( [0, 0, 0] ) )
+
+scene << PointLight.new( Vector.new( [-12,20,13] ), Colour.new( "White" ) )
+scene << PointLight.new( Vector.new( [12,-10,130] ), Colour.new( "White" ) )
+
+disc = Cylinder.new( Vector.new( [0,0,0] ), Vector.new( [0,0.1,0] ), 20 )
+disc_texture = Texture.new()
+disc_texture << "T_Glass1"
+disc_texture << "finish { Shiny }"
+disc << disc_texture
+disc << "interior { ior Flint_Glass_Ior }"
+
+scene << disc
+scene << spheres
+
+print scene
