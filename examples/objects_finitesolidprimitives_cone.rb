6efe50a2f8504b88a442994a907f15c57a5e90a4
Clive Crous <clive@crous.co.za>
2006-08-23 22:38:41 +0000

    [darcs @ better fit cone example into scene]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 06:35:34

--- examples/objects_finitesolidprimitives_cone.rb
+++ examples/objects_finitesolidprimitives_cone.rb
@@ -5,7 +5,7 @@ scene = Scene.new()
 
 cone =
     Povray::Objects::FiniteSolidPrimitives::Cone.new(
-        Povray::DataTypes::Vector::XYZ.new(0,0,0), 0.8,
+        Povray::DataTypes::Vector::XYZ.new(0,0,0), 0.6,
         Povray::DataTypes::Vector::XYZ.new(0,1,0), 0.3 )
 
 cone << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
