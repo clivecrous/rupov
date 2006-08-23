1d23aa0fe3363fef98435c858d016a03e4119a18
Clive Crous <clive@crous.co.za>
2006-08-23 22:38:05 +0000

    [darcs @ add Wall class to example scene]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-26 14:58:33

--- examples/include/genericscene.rb
+++ examples/include/genericscene.rb
@@ -1,11 +1,35 @@
 class Scene < Povray::Group
+    class Wall < Povray::Group
+        include Povray
+        include Objects
+        include InfiniteSolidPrimitives
+        include Textures
+        include Pigments
+        include Methods
+        def initialize( normal, offset)
+            super()
+            wall = Plane.new( normal, offset)
+            texture = Texture.new()
+            texture << SolidColour.new( Colour.new( DataTypes::Vector::RGB.new(1,0.95,0.9) ) )
+            normalCrackle = Normal.new()
+            normalCrackle << MultiValue.new([0.4],'crackle')
+            normalCrackle << MultiValue.new([0.05],'scale')
+            texture << normalCrackle
+            finish = Finish.new()
+            finish << MultiValue.new([0.5],'phong')
+            finish << MultiValue.new([0.5],'roughness')
+            texture << finish
+            wall << texture
+            self << wall
+        end
+    end
+    
     include Povray
     include Objects
     include InfiniteSolidPrimitives
     include Textures
     include Pigments
     include Methods
-
     def initialize
         super()
         self << '#include "colors.inc"'
