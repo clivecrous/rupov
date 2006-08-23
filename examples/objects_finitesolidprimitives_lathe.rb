8b6c5e4699b53bbb9c8cf922adc657ca67f27844
Clive Crous <clive@crous.co.za>
2006-08-23 22:37:39 +0000

    [darcs @ add lathe example]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-26 13:01:21

---
+++ examples/objects_finitesolidprimitives_lathe.rb
@@ -0,0 +1,22 @@
+require '../rupov/povray.rb'
+require 'include/genericscene.rb'
+
+scene = Scene.new()
+
+lathe =
+    Povray::Objects::FiniteSolidPrimitives::Lathe.new(
+        [
+            Povray::DataTypes::Vector::XY.new( 0,0 ),
+            Povray::DataTypes::Vector::XY.new( 1,1 ),
+            Povray::DataTypes::Vector::XY.new( 2,3 ),
+            Povray::DataTypes::Vector::XY.new( 2,4 ),
+            Povray::DataTypes::Vector::XY.new( 2,5 ),
+            Povray::DataTypes::Vector::XY.new( 0,4.75 )
+        ] )
+
+lathe << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+lathe << Povray::Methods::MultiValue.new( [0.2], 'scale' )
+
+scene << lathe
+
+puts scene
