d06600714e45ebc016a0d40bf4ce9deac85f288b
Clive Crous <clive@crous.co.za>
2006-08-23 22:39:08 +0000

    [darcs @ cylinder example now uses blocks]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 10:14:41

--- examples/objects_finitesolidprimitives_cylinder.rb
+++ examples/objects_finitesolidprimitives_cylinder.rb
@@ -1,16 +1,13 @@
 require '../rupov/povray.rb'
 require 'include/genericscene.rb'
 
-scene = Scene.new()
-
-cylinder =
-    Povray::Objects::FiniteSolidPrimitives::Cylinder.new(
-        Povray::DataTypes::Vector::XYZ.new( 0,0,0),
-        Povray::DataTypes::Vector::XYZ.new(0,1,0),
-        0.5 )
-
-cylinder << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
-
-scene << cylinder
+scene = Scene.new() { |scene|
+    scene << Povray::Objects::FiniteSolidPrimitives::Cylinder.new(
+            Povray::DataTypes::Vector::XYZ.new( 0,0,0),
+            Povray::DataTypes::Vector::XYZ.new(0,1,0),
+            0.5 ) { |cylinder|
+        cylinder << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+    }
+}
 
 puts scene
