616a9677e69d7bf16ff5f8e3529eba19243afd84
Clive Crous <clive@crous.co.za>
2006-08-23 22:42:03 +0000

    [darcs @ add spheresweep example]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 16:22:04

---
+++ examples/objects_finitesolidprimitives_spheresweep.rb
@@ -0,0 +1,19 @@
+require '../rupov/povray.rb'
+require 'include/genericscene.rb'
+
+scene = Scene.new() { |scene|
+
+    scene << Povray::Objects::FiniteSolidPrimitives::SphereSweep.new(
+            [   Povray::DataTypes::Vector::XYZ.new( 0, -0.1, 0), 0.1,
+                Povray::DataTypes::Vector::XYZ.new( 0, 0, 0), 0.1,
+                Povray::DataTypes::Vector::XYZ.new( -0.5, 0.25, 0), 0.075,
+                Povray::DataTypes::Vector::XYZ.new( -0.5, 0.5, 0.5), 0.05,
+                Povray::DataTypes::Vector::XYZ.new( 0.5, 0.75, 0.5), 0.025,
+                Povray::DataTypes::Vector::XYZ.new( 0.5, 1, -0.5), 0.001,
+                Povray::DataTypes::Vector::XYZ.new( 0, 1, 0), 0
+            ], 'b_spline' ) { |spheresweep|
+        spheresweep << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+    }
+}
+
+puts scene
