39c256c3beafa72f095a3bff9f8b8cff2cd8dd13
Clive Crous <clive@crous.co.za>
2006-08-23 22:37:23 +0000

    [darcs @ add polygon example]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-26 12:40:22

---
+++ examples/objects_finitepatchprimitives_polygon.rb
@@ -0,0 +1,28 @@
+require '../rupov/povray.rb'
+require 'include/genericscene.rb'
+
+scene = Scene.new()
+
+polygon =
+    Povray::Objects::FinitePatchPrimitives::Polygon.new(
+        [   Povray::DataTypes::Vector::XY.new( 0,0 ),
+            Povray::DataTypes::Vector::XY.new( 0,6 ),
+            Povray::DataTypes::Vector::XY.new( 4,6 ),
+            Povray::DataTypes::Vector::XY.new( 4,3 ),
+            Povray::DataTypes::Vector::XY.new( 1,3 ),
+            Povray::DataTypes::Vector::XY.new( 1,0 ),
+            Povray::DataTypes::Vector::XY.new( 0,0 ),
+            Povray::DataTypes::Vector::XY.new( 1,4 ),
+            Povray::DataTypes::Vector::XY.new( 1,5 ),
+            Povray::DataTypes::Vector::XY.new( 3,5 ),
+            Povray::DataTypes::Vector::XY.new( 3,4 ),
+            Povray::DataTypes::Vector::XY.new( 1,4 )
+        ] )
+
+polygon << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+polygon << Povray::Methods::MultiValue.new( [0.2], "scale" )
+polygon << Povray::Methods::MultiValue.new( [Povray::DataTypes::Vector::XYZ.new( 0,225,0 )], "rotate" )
+
+scene << polygon
+
+puts scene
