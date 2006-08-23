2e57cf4ee99173c9c7d4ac0a72475982ee6eae96
Clive Crous <clive@crous.co.za>
2006-08-23 22:43:25 +0000

    [darcs @ move data centric configuration out to a config file for geoeas example]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-03-21 20:53:02

--- examples/movies/geoeas.rb
+++ examples/movies/geoeas.rb
@@ -1,21 +1,55 @@
 require '../../rupov/povray.rb'
 
-Frames = 200 
-MaxColours = 2000
+if ARGV.length < 1
+    puts "You need to supply a data filename."
+    exit
+end
+
+ConfigFilename = "#{ARGV[0]}.rb"
+DataFilename = "#{ARGV[0]}.GeoEAS"
+
+begin
+    open(ConfigFilename)
+rescue
+    puts "Unable to find configuration file: #{ConfigFilename}"
+    exit
+end
+
+require ConfigFilename
 
 class GeoEAS < Array
     attr_reader :title, :columnAmount, :columns
     def initialize( filename )
-        file = File.new( filename )
+        begin
+            file = File.new( filename )
+        rescue
+            puts "Unable to load GeoEAS data file: #{DataFilename}"
+            exit
+        end
         @title = file.readline.strip
         @columnAmount = file.readline.to_i
+        raise "Only 4 colounm GeoEAS data is supported at the moment" if @columnAmount != 4
         @columns=[]
+        columnMin=[ 10e100]*4
+        columnMax=[-10e100]*4
         (1..@columnAmount).each { @columns << file.readline.strip }
-        self.concat file.readlines.map { |line| line.strip.split }.select { |a,b,c,d| d != "-999" }.map { |element| element.map {|a| a.to_f} }
+        self.concat file.readlines.map { |line| line.strip.split }.select { |a,b,c,d| d != "-999" }.map { |element| element.map {|a| a.to_f} }.each { |line|
+            line.each_index do |index|
+                columnMin[index] = line[index] if line[index] < columnMin[index]
+                columnMax[index] = line[index] if line[index] > columnMax[index]
+            end
+        }
+        columnMax.each_index { |index| columnMax[index]-=columnMin[index] }
+        self.map do |line|
+            line.each_index do |index|
+                line[index]=((line[index]-columnMin[index])/columnMax[index])-0.5
+            end
+            line
+        end
     end
 end
 
-geo = GeoEAS.new("statistical_data.GeoEAS")
+geo = GeoEAS.new(DataFilename)
 
 colours = []
 (0..MaxColours).each { |colournum|
@@ -40,29 +74,26 @@ colours = []
 $threshold = "0"
 $rotation = "0*y"
 
-StrengthPower = 1.75
-
 puts "Generating scene..."
 scene = Povray::Group.new { |scene|
-        scene << Povray::Group.new() { |prerender|
+        scene << Povray::Group.new { |prerender|
             prerender << Povray::Methods::Include.new('colors.inc')
             prerender << Povray::Camera::Basic.new(
-                        Povray::Methods::Location.new( Povray::DataTypes::Vector::XYZ.new(1,1.75,2) ),
-                        Povray::Methods::LookAt.new( Povray::DataTypes::Vector::XYZ.new(0,0.25,0) ) )
+                        Povray::Methods::Location.new( Povray::DataTypes::Vector::XYZ.new(0.8*CameraDistance,0.8*CameraDistance,1.2*CameraDistance) ),
+                        Povray::Methods::LookAt.new( Povray::DataTypes::Vector::XYZ.new(0,0,0) ) )
             prerender << Povray::LightSources::PointLight.new(
-                        Povray::DataTypes::Vector::XYZ.new( 5,5,2 ), Povray::Methods::Colour.new( "White" ) )
+                        Povray::DataTypes::Vector::XYZ.new( 5.0*LightDistance,5.0*LightDistance,2.0*LightDistance ), Povray::Methods::Colour.new( "White" ) )
             prerender << Povray::Atmosphere::Background.new( Povray::Methods::Colour.new( "White" ) )
         }.to_s
 
         scene << Povray::Objects::FiniteSolidPrimitives::Blob.new( $threshold ) { |blob|
-            blob << Povray::Group.new() { |prerender|
+            blob << Povray::Group.new { |prerender|
                 geo.each { |(x,y,z,mean)|
-                    x=x/75-0.8; y=y/75-0.8; z/=20
-                    colour = (mean**1.1).to_i
+                    colour = ((mean+0.5)*MaxColours).to_i
                     colour=MaxColours if colour > MaxColours
                     prerender << Povray::Objects::FiniteSolidPrimitives::Sphere.new(
-                       Povray::DataTypes::Vector::XYZ.new( x,z,y ), 0.1 ) { |sphere|
-                           sphere << (mean.to_f**StrengthPower)/(MaxColours**StrengthPower)
+                       Povray::DataTypes::Vector::XYZ.new( x*Scale1,z*Scale3,y*Scale2 ), Radius ) { |sphere|
+                           sphere << (((mean+0.5)*MaxColours)**StrengthPower)/(MaxColours**StrengthPower)
                            sphere << colours[colour]
                     }
                 }
---
+++ examples/movies/statistical_data.rb
@@ -0,0 +1,12 @@
+Frames = 200
+MaxColours = 2000
+StrengthPower = 2.5
+
+CameraDistance = 1.0
+LightDistance = CameraDistance
+
+Radius = 0.075
+
+Scale1 = 1.0
+Scale2 = 1.0
+Scale3 = 0.5
