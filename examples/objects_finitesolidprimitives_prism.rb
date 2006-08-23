b58ed595bf58a0e8c27f2c11b06efccd4bf0cd21
Clive Crous <clive@crous.co.za>
2006-08-23 22:39:14 +0000

    [darcs @ prism example now uses blocks]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 10:17:17

--- examples/objects_finitesolidprimitives_prism.rb
+++ examples/objects_finitesolidprimitives_prism.rb
@@ -1,25 +1,22 @@
 require '../rupov/povray.rb'
 require 'include/genericscene.rb'
 
-scene = Scene.new()
-
-prism =
-    Povray::Objects::FiniteSolidPrimitives::Prism.new(
-        0,1,
-        [
-            Povray::DataTypes::Vector::XY.new( 3,5 ),
-            Povray::DataTypes::Vector::XY.new(-3,5 ),
-            Povray::DataTypes::Vector::XY.new(-5,0 ),
-            Povray::DataTypes::Vector::XY.new(-3,-5 ),
-            Povray::DataTypes::Vector::XY.new( 3,-5 ),
-            Povray::DataTypes::Vector::XY.new( 5,0 ),
-            Povray::DataTypes::Vector::XY.new( 3,5 )
-        ] )
-
-prism << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
-prism << Povray::Methods::Scale.new( 0.15 )
-prism << Povray::Methods::Translate.new( Povray::DataTypes::Vector::XYZ.new(-0.2,0,-0.2) )
-
-scene << prism
+scene = Scene.new() { |scene|
+    scene << Povray::Objects::FiniteSolidPrimitives::Prism.new(
+            0,1,
+            [
+                Povray::DataTypes::Vector::XY.new( 3,5 ),
+                Povray::DataTypes::Vector::XY.new(-3,5 ),
+                Povray::DataTypes::Vector::XY.new(-5,0 ),
+                Povray::DataTypes::Vector::XY.new(-3,-5 ),
+                Povray::DataTypes::Vector::XY.new( 3,-5 ),
+                Povray::DataTypes::Vector::XY.new( 5,0 ),
+                Povray::DataTypes::Vector::XY.new( 3,5 )
+            ] ) { |prism|
+        prism << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+        prism << Povray::Methods::Scale.new( 0.15 )
+        prism << Povray::Methods::Translate.new( Povray::DataTypes::Vector::XYZ.new(-0.2,0,-0.2) )
+    }
+}
 
 puts scene
