6b775db490293b2fc3795259c07f2fb7192f000a
Clive Crous <clive@crous.co.za>
2006-08-23 22:39:17 +0000

    [darcs @ spere example now uses blocks]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 10:21:26

--- examples/objects_finitesolidprimitives_sphere.rb
+++ examples/objects_finitesolidprimitives_sphere.rb
@@ -1,15 +1,13 @@
 require '../rupov/povray.rb'
 require 'include/genericscene.rb'
 
-scene = Scene.new()
+scene = Scene.new() { |scene|
 
-sphere =
-    Povray::Objects::FiniteSolidPrimitives::Sphere.new(
-        Povray::DataTypes::Vector::XYZ.new( 0, 0.5, 0),
-        0.45 )
-
-sphere << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
-
-scene << sphere
+    scene << Povray::Objects::FiniteSolidPrimitives::Sphere.new(
+            Povray::DataTypes::Vector::XYZ.new( 0, 0.5, 0),
+            0.45 ) { |sphere|
+        sphere << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+    }
+}
 
 puts scene
