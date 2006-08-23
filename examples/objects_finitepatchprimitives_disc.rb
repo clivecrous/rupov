88c448013a5b6f5ed96a82a38ed0835d4aaf6dee
Clive Crous <clive@crous.co.za>
2006-08-23 22:38:53 +0000

    [darcs @ disc example uses blocks now]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 09:18:02

--- examples/objects_finitepatchprimitives_disc.rb
+++ examples/objects_finitepatchprimitives_disc.rb
@@ -1,17 +1,14 @@
 require '../rupov/povray.rb'
 require 'include/genericscene.rb'
 
-scene = Scene.new()
-
-disc =
-    Povray::Objects::FinitePatchPrimitives::Disc.new(
+scene = Scene.new() { |scene|
+    scene << Povray::Objects::FinitePatchPrimitives::Disc.new(
         Povray::DataTypes::Vector::XYZ.new( 0,0.5,0),
         Povray::DataTypes::Vector::XYZ.new(0,1,0),
         0.7,
-        0.2)
-
-disc << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
-
-scene << disc
+        0.2) { |disc|
+            disc << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+        }
+    }
 
 puts scene
