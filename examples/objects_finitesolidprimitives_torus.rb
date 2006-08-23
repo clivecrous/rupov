2bbc67780b98fdf45832085c389bbe6266bd567e
Clive Crous <clive@crous.co.za>
2006-08-23 22:39:20 +0000

    [darcs @ torus example now uses blocks]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 10:21:41

--- examples/objects_finitesolidprimitives_torus.rb
+++ examples/objects_finitesolidprimitives_torus.rb
@@ -1,15 +1,11 @@
 require '../rupov/povray.rb'
 require 'include/genericscene.rb'
 
-scene = Scene.new()
-
-torus =
-    Povray::Objects::FiniteSolidPrimitives::Torus.new( 0.65, 0.2 )
-
-torus << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
-
-torus << Povray::Methods::Translate.new( Povray::DataTypes::Vector::XYZ.new( 0, 0.5, 0 ))
-
-scene << torus
+scene = Scene.new() { |scene|
+    scene << Povray::Objects::FiniteSolidPrimitives::Torus.new( 0.65, 0.2 ) { |torus|
+        torus << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+        torus << Povray::Methods::Translate.new( Povray::DataTypes::Vector::XYZ.new( 0, 0.5, 0 ))
+    }
+}
 
 puts scene
