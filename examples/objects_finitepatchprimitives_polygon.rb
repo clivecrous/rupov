b3722ca2071a67ced77fa87cd0b013e55dbd23d6
Clive Crous <clive@crous.co.za>
2006-08-23 22:39:05 +0000

    [darcs @ correct polygon example's indentation]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 10:14:03

--- examples/objects_finitepatchprimitives_polygon.rb
+++ examples/objects_finitepatchprimitives_polygon.rb
@@ -17,10 +17,10 @@ scene = Scene.new() { |scene|
                 Povray::DataTypes::Vector::XY.new( 3,4 ),
                 Povray::DataTypes::Vector::XY.new( 1,4 )
             ] ) { |polygon|
-                polygon << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
-                polygon << Povray::Methods::MultiValue.new( [0.2], "scale" )
-                polygon << Povray::Methods::MultiValue.new( [Povray::DataTypes::Vector::XYZ.new( 0,225,0 )], "rotate" )
-            }
+        polygon << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+        polygon << Povray::Methods::MultiValue.new( [0.2], "scale" )
+        polygon << Povray::Methods::MultiValue.new( [Povray::DataTypes::Vector::XYZ.new( 0,225,0 )], "rotate" )
+    }
 }
 
 puts scene
