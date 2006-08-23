f43fc52fb3c7a8fa5a9ec7dbb34d9657b3d13c82
Clive Crous <clive@crous.co.za>
2006-08-23 22:39:02 +0000

    [darcs @ cone example now uses blocks]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 10:10:46

--- examples/objects_finitesolidprimitives_cone.rb
+++ examples/objects_finitesolidprimitives_cone.rb
@@ -1,15 +1,12 @@
 require '../rupov/povray.rb'
 require 'include/genericscene.rb'
 
-scene = Scene.new()
-
-cone =
-    Povray::Objects::FiniteSolidPrimitives::Cone.new(
-        Povray::DataTypes::Vector::XYZ.new(0,0,0), 0.6,
-        Povray::DataTypes::Vector::XYZ.new(0,1,0), 0.3 )
-
-cone << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
-
-scene << cone
+scene = Scene.new() { |scene|
+    scene << Povray::Objects::FiniteSolidPrimitives::Cone.new(
+            Povray::DataTypes::Vector::XYZ.new(0,0,0), 0.6,
+            Povray::DataTypes::Vector::XYZ.new(0,1,0), 0.3 ) { |cone|
+        cone << Povray::Textures::Pigments::SolidColour.new( Povray::Methods::Colour.new( "Yellow" ) )
+    }
+}
 
 puts scene
