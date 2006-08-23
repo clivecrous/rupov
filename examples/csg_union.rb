ac3f7e72cabbd9f6d70b374f2bf19e38a897e8a5
Clive Crous <clive@crous.co.za>
2006-08-23 22:42:39 +0000

    [darcs @ add csg::union example]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-28 15:42:18

---
+++ examples/csg_union.rb
@@ -0,0 +1,17 @@
+require '../rupov/povray.rb'
+require 'include/genericscene.rb'
+
+puts Scene.new() { |scene|
+    scene << Povray::CSG::Union.new() { |union|
+        union << Povray::Objects::FiniteSolidPrimitives::Sphere.new(
+                Povray::DataTypes::Vector::XYZ.new( -0.15, 0.5, -0.15), 0.45 ) { |sphere|
+            sphere <<  Povray::Textures::Pigments::SolidColour.new(
+                    Povray::Methods::Colour.new( "Yellow" ) )
+        }
+        union << Povray::Objects::FiniteSolidPrimitives::Sphere.new(
+                Povray::DataTypes::Vector::XYZ.new(  0.15, 0.7,  0.15), 0.45 ) { |sphere|
+            sphere <<  Povray::Textures::Pigments::SolidColour.new(
+                    Povray::Methods::ColourRGBT.new( Povray::DataTypes::Vector::RGBT.new( 1,0,0,0.8 ) ) )
+        }
+    }
+}
