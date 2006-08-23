24f426fdd9027dee0266e1dacda7ab4fcd2481d2
Clive Crous <clive@crous.co.za>
2006-08-23 22:35:52 +0000

    [darcs @ add grass example]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-01-08 21:49:06

---
+++ examples/grass.rb
@@ -0,0 +1,58 @@
+require 'povray.rb'
+include Povray
+
+include Objects
+include FiniteSolidPrimitives
+include InfiniteSolidPrimitives
+include FinitePatchPrimitives
+
+include Atmosphere
+include Methods
+include LightSources
+include Textures
+
+$grassTexture = Texture.new()
+$grassTexture << Pigments::SolidColour.new( Colour.new( "Green" ) )
+
+def grassBlade( length, baseRadius, curve, segments)
+    blade = CSG::Union.new()
+
+    0.upto(segments) do |segment|
+        degrees = (segment*curve/segments)*Math::PI/180
+        x1 = Math::sin( degrees )*(segment*length/segments)
+        y1 = Math::cos( degrees )*(segment*length/segments)
+        degrees = ((segment+1)*curve/segments)*Math::PI/180
+        x2 = Math::sin( degrees )*((segment+1)*length/segments)
+        y2 = Math::cos( degrees )*((segment+1)*length/segments)
+        
+        cone = Cone.new( Vector.new( [x1,y1,0] ), (segments-segment)*baseRadius/segments, Vector.new( [x2,y2,0] ), (segments-(segment+1))*baseRadius/segments)
+        cone << $grassTexture
+        blade << cone
+    end
+
+    blade
+end
+
+scene = Povray::Group.new()
+
+scene << '#include "colors.inc"'
+
+scene << Background.new( Colour.new( "Black" ) )
+scene << Camera::Basic.new( Location.new( [35, 20, 50] ), LookAt.new( [25, 0, 15] ) )
+
+scene << PointLight.new( Vector.new( [10,30,10] ), Colour.new( "White" ) )
+scene << PointLight.new( Vector.new( [0,30,10] ), Colour.new( "White" ) )
+
+ground = Plane.new( Vector.new( [0,1,0] ), 0 )
+ground << Pigments::SolidColour.new( ColourRGB.new( [0.6,0.4,0] ) )
+
+scene << ground
+
+(0..1500).each do |num|
+    grass =grassBlade( 8+rand()*4, 0.1, 25+rand()*20, 10)
+    grass << Rotate.new( [0,rand()*360,0] )
+    grass << Translate.new( [rand()*50,0,rand()*50] )
+    scene << grass
+end
+
+print scene
