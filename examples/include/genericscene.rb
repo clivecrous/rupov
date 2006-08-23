099940f8e28cc61ceb2a0f0a936288eef7ede30f
Clive Crous <clive@crous.co.za>
2006-08-23 22:38:50 +0000

    [darcs @ genericscene for examples now uses blocks]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-02-27 09:16:05

--- examples/include/genericscene.rb
+++ examples/include/genericscene.rb
@@ -8,56 +8,54 @@ class Scene < Povray::Group
         include Methods
         def initialize( rotate, translate )
             super()
-            entireWall = CSG::Union.new()
-            wall = Plane.new( DataTypes::Vector::XYZ.new(0,0,-1),0 )
-            texture = Texture.new()
-            texture << SolidColour.new( Colour.new( DataTypes::Vector::RGB.new(1,0.95,0.9) ) )
-            normalCrackle = Normal.new()
-            normalCrackle << MultiValue.new([0.4],'crackle')
-            normalCrackle << MultiValue.new([0.05],'scale')
-            texture << normalCrackle
-            finish = Finish.new()
-            finish << MultiValue.new([0.5],'phong')
-            finish << MultiValue.new([0.5],'roughness')
-            texture << finish
-            wall << texture
+            entireWall = CSG::Union.new() { |entireWall|
+                entireWall << Plane.new( DataTypes::Vector::XYZ.new(0,0,-1),0 ) { |wall|
+                    wall << Texture.new() { |texture|
+                        texture << SolidColour.new( Colour.new( DataTypes::Vector::RGB.new(1,0.95,0.9) ) )
+                        texture << Normal.new() { |normal|
+                            normal << MultiValue.new([0.4],'crackle')
+                            normal << MultiValue.new([0.05],'scale')
+                        }
+                        texture << Finish.new() { |finish|
+                            finish << MultiValue.new([0.5],'phong')
+                            finish << MultiValue.new([0.5],'roughness')
+                        }
+                    }
+                }
 
-            sideboard = CSG::Difference.new()
-            sideboard << FiniteSolidPrimitives::Box.new(
-                DataTypes::Vector::XYZ.new(-50,0,0),
-                DataTypes::Vector::XYZ.new( 50,0.75,0.3) )
-            sideboard_miniblock = FiniteSolidPrimitives::Box.new(
-                DataTypes::Vector::XYZ.new(-50,0.6,0.15),
-                DataTypes::Vector::XYZ.new( 50,0.751,0.31) )
-            sideboard_cutout = CSG::Difference.new()
-            sideboard_cutoutinner = CSG::Intersection.new()
-            sideboard_cutoutinner << sideboard_miniblock
-            sideboard_cutoutinner << FiniteSolidPrimitives::Cylinder.new(
-                DataTypes::Vector::XYZ.new(-50,0.6,0.15),
-                DataTypes::Vector::XYZ.new( 50,0.6,0.15), 0.15 )
-            sideboard_cutout << sideboard_miniblock
-            sideboard_cutout << sideboard_cutoutinner
-            sideboard << sideboard_cutout
+                entireWall << CSG::Difference.new() { |sideboard|
+                    sideboard_miniblock = FiniteSolidPrimitives::Box.new(
+                        DataTypes::Vector::XYZ.new(-50,0.6,0.15),
+                        DataTypes::Vector::XYZ.new( 50,0.751,0.31) )
 
-            texture = Texture.new()
-            pigment = Pigment.new()
-            pigment << "P_WoodGrain3A"
-            colour_map = Base.new('colour_map')
-            colour_map << "M_Teak"
-            pigment << colour_map
-            pigment << Scale.new( 0.75 )
-            pigment << Rotate.new( DataTypes::Vector::XYZ.new(2,85,5) )
-            pigment << Translate.new( DataTypes::Vector::XYZ.new(100,200,300) )
-            texture << pigment
+                    sideboard << FiniteSolidPrimitives::Box.new(
+                        DataTypes::Vector::XYZ.new(-50,0,0),
+                        DataTypes::Vector::XYZ.new( 50,0.75,0.3) )
+                    sideboard << CSG::Difference.new() { |sideboard_cutout|
+                        sideboard_cutout << sideboard_miniblock
+                        sideboard_cutout << CSG::Intersection.new() { |sideboard_cutoutinner|
+                            sideboard_cutoutinner << sideboard_miniblock
+                            sideboard_cutoutinner << FiniteSolidPrimitives::Cylinder.new(
+                                DataTypes::Vector::XYZ.new(-50,0.6,0.15),
+                                DataTypes::Vector::XYZ.new( 50,0.6,0.15), 0.15 )
+                                }
+                    }
 
-            sideboard << texture
-            sideboard_cutout << texture
-            
-            entireWall << wall
-            entireWall << sideboard
-            
-            entireWall << Rotate.new( rotate )
-            entireWall << Translate.new( translate )
+                    sideboard << Texture.new() { |texture|
+                        texture << Pigment.new() { |pigment|
+                            pigment << "P_WoodGrain3A"
+                            pigment << Base.new("colour_map") { |colourmap|
+                                colourmap << "M_Teak"
+                            }
+                            pigment << Scale.new( 0.75 )
+                            pigment << Rotate.new( DataTypes::Vector::XYZ.new(2,85,5) )
+                            pigment << Translate.new( DataTypes::Vector::XYZ.new(100,200,300) )
+                        }
+                    }
+                }
+                entireWall << Rotate.new( rotate )
+                entireWall << Translate.new( translate )
+            }
 
             self << entireWall
         end
@@ -74,9 +72,9 @@ class Scene < Povray::Group
         self << Include.new('colors.inc')
         self << Include.new('woods.inc')
         self << Include.new('teak.map')
-        global_settings = Base.new('global_settings')
-        radiosity = Base.new('radiosity')
-        global_settings << radiosity
+        global_settings = Base.new('global_settings') { |this|
+            this << Base.new('radiosity')
+        }
         self << global_settings
 
         self << Camera::Basic.new(
@@ -86,15 +84,15 @@ class Scene < Povray::Group
         self << LightSources::PointLight.new(
                     Povray::DataTypes::Vector::XYZ.new( 5,5,2 ), Colour.new( "White" ) )
 
-        floor = Plane.new( Povray::DataTypes::Vector::XYZ.new( 0,1,0 ), 0)
-        checker = Texture.new()
-        checker << Checker.new( Colour.new( "White" ), Colour.new( "Blue" ) )
-        checker << Scale.new( 1.7 )
-        finish = Finish.new()
-        finish << "reflection { 0.3 }"
-        checker << finish
-        floor << checker
-
+        floor = Plane.new( Povray::DataTypes::Vector::XYZ.new( 0,1,0 ), 0) { |this|
+            this << Texture.new() { |this|
+                this << Checker.new( Colour.new( "White" ), Colour.new( "Blue" ) )
+                this << Scale.new( 1.7 )
+                this << Finish.new() { |this|
+                    this << "reflection { 0.3 }"
+                }
+            }
+        }
         self << floor 
 
         self << Wall.new(
@@ -103,5 +101,7 @@ class Scene < Povray::Group
         self << Wall.new(
                     Povray::DataTypes::Vector::XYZ.new( 0,90,0),
                     Povray::DataTypes::Vector::XYZ.new(-2,0,0) )
+
+        yield(self) if block_given? and self.class == Scene
     end
 end
