b816a461bf19b622a763968e3dae32b0fd15f5d9
Clive Crous <clive@crous.co.za>
2006-08-23 22:36:41 +0000

    [darcs @ add cylinder example]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-01-26 20:38:29

---
+++ examples/objects_finitesolidprimitives_cylinder.rb
@@ -0,0 +1,14 @@
+require '../rupov/povray.rb'
+require 'include/genericscene.rb'
+
+include Povray::Objects::FiniteSolidPrimitives
+include Povray::Textures::Pigments
+include Povray::Methods
+
+scene = Scene.new()
+
+cylinder = Cylinder.new( Povray::DataTypes::Vector::ThreeD.new( 0,0,0) , Povray::DataTypes::Vector::ThreeD.new(0,1,0), 0.5 )
+cylinder << SolidColour.new( Colour.new( "Yellow" ) )
+scene << cylinder
+
+puts scene
