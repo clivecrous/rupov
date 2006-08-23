28d9ef98ab287def66eb020bad4d5891f0f4363c
Clive Crous <clive@crous.co.za>
2006-08-23 22:36:59 +0000

    [darcs @ tweak the grass example to show off the grass a bit better]
    Original author: Clive Crous <clive@darkarts.co.za>
    Date: 2006-01-26 21:43:42

--- examples/misc/grass.rb
+++ examples/misc/grass.rb
@@ -12,16 +12,20 @@ include LightSources
 include Textures
 
 $grassTexture = Texture.new()
-$grassTexture << Pigments::SolidColour.new( Colour.new( "Green" ) )
+$grassColour = Povray::DataTypes::Vector::Colour.new( 0, 1, 0)
+$grassTexture << Pigments::SolidColour.new( ColourRGB.new( $grassColour ) )
 
 def grassBlade( length, baseRadius, curve, segments)
     blade = CSG::Union.new()
 
+    $grassColour.g = rand()*0.4 + 0.6
+    $grassColour.r = rand()*0.4
+
+    degrees = 0
     0.upto(segments) do |segment|
-        degrees = (segment*curve/segments)*Math::PI/180
         x1 = Math::sin( degrees )*(segment*length/segments)
         y1 = Math::cos( degrees )*(segment*length/segments)
-        degrees = ((segment+1)*curve/segments)*Math::PI/180
+        degrees += ((curve*(rand()*0.6+0.6))/segments)*Math::PI/180
         x2 = Math::sin( degrees )*((segment+1)*length/segments)
         y2 = Math::cos( degrees )*((segment+1)*length/segments)
         
@@ -32,6 +36,8 @@ def grassBlade( length, baseRadius, curve, segments)
             (segments-(segment+1))*baseRadius/segments)
         cone << $grassTexture
         blade << cone
+        $grassColour.r-=0.05 if $grassColour.r > 0
+        $grassColour.g-=0.01 if $grassColour.g > 0
     end
 
     blade
@@ -42,9 +48,9 @@ scene = Povray::Group.new()
 scene << '#include "colors.inc"'
 
 scene << Background.new( Colour.new( "Black" ) )
-scene << Camera::Basic.new( Location.new( 35, 20, 50 ), LookAt.new( 25, 0, 15 ) )
+scene << Camera::Basic.new( Location.new( 35, 20, 50 ), LookAt.new( 25, -15, 15 ) )
 
-scene << PointLight.new( Povray::DataTypes::Vector::ThreeD.new( 10,30,10 ), Colour.new( "White" ) )
+scene << PointLight.new( Povray::DataTypes::Vector::ThreeD.new( 20,30,40 ), Colour.new( "White" ) )
 scene << PointLight.new( Povray::DataTypes::Vector::ThreeD.new( 0,30,10 ), Colour.new( "White" ) )
 
 ground = Plane.new( Povray::DataTypes::Vector::ThreeD.new(0,1,0), 0 )
@@ -54,9 +60,9 @@ scene << ground
 
 print scene
 
-(0..1500).each do |num|
-    grass =grassBlade( 8+rand()*4, 0.1, 25+rand()*20, 10)
-    grass << Rotate.new( 0,rand()*360,0 )
+(0..3500).each do |num|
+    grass =grassBlade( 8+rand()*4, 0.1, 25+rand()*20, 20)
+    grass << Rotate.new( 0,rand()*90+110,0 )
     grass << Translate.new( rand()*50,0,rand()*50 )
     print grass
 end
